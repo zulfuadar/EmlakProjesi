8d01e7ab75083363aa49ba84348d07dc46792ab0
Kerem <keremdincel1414@gmail.com>
2016-02-27 18:48:02 +0200

    no message

--- GÜRSOYEMLAK1/Admin Paneli.vb
+++ GÜRSOYEMLAK1/Admin Paneli.vb
@@ -6,6 +6,9 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
         Form1.Show()
+        'asdasdasdasdasddasdasd'
+        'asddddddd'
+
 
     End Sub
 
